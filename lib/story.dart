5b19cec9f71298f4eff431cbdf0cb236260747ac
Balkis998 <balqeesturk3@gmail.com>
2021-06-01 22:08:13 +0300

    test

---
+++ lib/story.dart
@@ -0,0 +1,8 @@
+class Story {
+  String storyText, firstChoice, secondChoice;
+  Story({String storyTitle, String choice1, String choice2}) {
+    storyText = storyTitle;
+    firstChoice = choice1;
+    secondChoice = choice2;
+  }
+}
